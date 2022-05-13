115a3287bd5ccf76e296327b7fdaf0bcf9ee19b6
jaumpedro214 <joaopedrodasilvalima@gmail.com>
2022-05-13 15:48:19 -0300

    Main jupyter notebook & Python Scripts

---
+++ ensamble_clustering/simlarity_matrix.py
@@ -0,0 +1,44 @@
+from scipy.spatial.distance import cdist
+import numpy as np
+
+class ClusterSimilarityMatrix():
+    
+    def __init__(self) -> None:
+        self._is_fitted = False
+
+    def fit(self, y_clusters):
+        if not self._is_fitted:
+            self._is_fitted = True
+            self.similarity = self.to_binary_matrix(y_clusters)
+            return self
+
+        self.similarity += self.to_binary_matrix(y_clusters)
+
+    def to_binary_matrix(self, y_clusters):
+        y_reshaped = np.expand_dims(y_clusters, axis=-1)
+        return (cdist(y_reshaped, y_reshaped, 'cityblock')==0).astype(int)
+
+
+class EnsambleCustering():
+    def __init__(self, base_estimators, aggregator, distances=False):
+        self.base_estimators = base_estimators
+        self.aggregator = aggregator
+        self.distances = distances
+
+    def fit(self, X):
+        X_ = X.copy()
+
+        clt_sim_matrix = ClusterSimilarityMatrix()
+        for model in self.base_estimators:
+            clt_sim_matrix.fit(model.fit_predict(X=X_))
+        
+        sim_matrix = clt_sim_matrix.similarity
+        self.cluster_matrix = sim_matrix/sim_matrix.diagonal()
+
+        if self.distances:
+            self.cluster_matrix = np.abs(np.log(self.cluster_matrix + 1e-8)) # Avoid log(0)
+
+    def fit_predict(self, X):
+        self.fit(X)
+        y = self.aggregator.fit_predict(self.cluster_matrix)
+        return y
