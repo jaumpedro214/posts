a4c44e0d2198fe2382059cf53236d77e5639b0ae
joaopedro214 <joaopedrodasilvalima@gmail.com>
2023-02-26 11:26:38 -0300

    FEAT: Download Files from INEP - Censo

---
+++ data_pipeline_spark_gcs_gbq/download_files.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+BASE_URL = "https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_{}.zip"
+YEARS = range(2011, 2021)
+SAVE_PATH = os.path.join(os.path.dirname(__file__), "data")
+UNZIP = True
+
+if __name__ == "__main__":
+    for year in YEARS:
+        print("Downloading data for year {}".format(year))
+        url = BASE_URL.format(year)
+        r = requests.get(url, allow_redirects=True, verify=False)
+        open(os.path.join(SAVE_PATH, "{}.zip".format(year)), "wb").write(r.content)
+
+        # Unzip the file
+        if UNZIP:
+            print("Unzipping file")
+            os.system(f"unzip {SAVE_PATH}/{year}.zip -d {SAVE_PATH}/{year}")
+
