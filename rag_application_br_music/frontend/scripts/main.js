93e9b3b0fd376bd1ec3e6c35607c34c54860ccf1
joaopedro <joaopedrodasilvalima@gmail.com>
2025-10-13 22:31:49 -0300

    FEAT: Mock chat response

--- rag_application_br_music/frontend/scripts/main.js
+++ rag_application_br_music/frontend/scripts/main.js
@@ -37,16 +37,55 @@ function userQuestionFlow() {
     }
 
     saveUserQuestionToLocalStorage(userQuestion);
-    
     goToChatPage();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+
+function requestChatResponse(userQuestion) {
+    response = `This is a mock response for  the question.
+
+    Question: ${userQuestion}
+    `;
+
+    // create the <p> elements for each \n
+
+    response = response.split("\n").map( line => `<p>${line}</p>` ).join("");
+    return response;
+}
+
+function requestChatRAGResponse(userQuestion) {
+    response = `This is a mock response for  the question.
+
+    Question: ${userQuestion}
+    `;
+
+    // create the <p> elements for each \n
+
+    response = response.split("\n").map( line => `<p>${line}</p>` ).join("");
+    return response;
+}
+
+
+function userQuestionResponseFlow() {
     const userQuestion = getUserQuestionFromLocalStorage();
 
-    if (userQuestion) {
-        writeUserQuestionToChatPage(userQuestion);
-        localStorage.removeItem("userQuestion");
+    if (!userQuestion){
+        return;
     }
 
-});
+    writeUserQuestionToChatPage(userQuestion);
+
+    const chatResponse = requestChatResponse(userQuestion);
+    const chatRAGResponse = requestChatRAGResponse(userQuestion);
+
+    const chatReponseElement = document.querySelector(".js-chat-simple-response-text");
+    const chatRAGResopnseElement = document.querySelector(".js-chat-rag-response-text");
+
+    // include the responses 
+    chatReponseElement.innerHTML = chatResponse;
+    chatRAGResopnseElement.innerHTML = chatRAGResponse;
+
+    localStorage.removeItem("userQuestion");
+}
+
+window.addEventListener("DOMContentLoaded", userQuestionResponseFlow);
