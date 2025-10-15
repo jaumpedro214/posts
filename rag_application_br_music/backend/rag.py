557b01bb71761cb32aca3ecb5e50a94da875578d
joaopedro <joaopedrodasilvalima@gmail.com>
2025-10-15 19:12:08 -0300

    FEAT: Finish RAG response pipeline

--- rag_application_br_music/backend/rag.py
+++ rag_application_br_music/backend/rag.py
@@ -21,7 +21,7 @@ def generate_response(question: str):
     client = get_embedding_api_client()
 
     response = client.responses.create(
-        model="o4-mini",
+        model="gpt-4o-mini",
             input=[
                 {
                     "role": "user",
@@ -35,19 +35,44 @@ def generate_response(question: str):
             ]
     )
 
-    print(response.output_text)
     return response.output_text
 
 def generate_rag_response(question: str):
-
     similar_documents = retrieve_similar_documents_from_database(question)
+    client = get_embedding_api_client()
+
+    song_lyrics = '\n\n'.join(
+        [
+            f"{doc['author'].replace('-', ' ').capitalize()}-{doc['name']}\n{doc['lyrics']}" 
+            for doc in similar_documents
+        ]
+    )
+
+    response = client.responses.create(
+        model="gpt-4o-mini",
+            input=[
+                {
+                    "role": "system",
+                    "content": f"Answer the user questions using the following data as content:\n{song_lyrics}" 
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "input_text",
+                            "text": question,
+                        },
+                    ]
+                },
+            ]
+    )
 
     return {
         'docs': similar_documents,
-        'content': question
+        'content': response.output_text
     }
 
-def retrieve_similar_documents_from_database(question: str, threshold=0.5, num_docs=2):
+def retrieve_similar_documents_from_database(question: str, threshold=0.5, num_docs=5):
     
     question_vector = embed_question(question)
     conn = connect_to_postgresql()
@@ -57,7 +82,7 @@ def retrieve_similar_documents_from_database(question: str, threshold=0.5, num_d
     sql_documents = f"""
         SELECT name, lyrics, author, (embedding <=> \'{question_vector}\') AS distance
         FROM songs
-        WHERE (embedding <=> \'{question_vector}\') > {threshold}
+        WHERE (embedding <=> \'{question_vector}\') < {threshold}
         ORDER BY embedding <=> \'{question_vector}\'
         LIMIT {num_docs}
     """
