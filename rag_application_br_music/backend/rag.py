545cdc42a7578a80ef9cbbfb7475814b32fbb7a3
joaopedro <joaopedrodasilvalima@gmail.com>
2025-10-15 17:45:02 -0300

    FEAT: Retrieve similar documents from the database

--- rag_application_br_music/backend/rag.py
+++ rag_application_br_music/backend/rag.py
@@ -39,16 +39,61 @@ def generate_response(question: str):
     return response.output_text
 
 def generate_rag_response(question: str):
-    return question
 
-def retrieve_similar_documents_from_database(question: str):
-    pass
+    similar_documents = retrieve_similar_documents_from_database(question)
+
+    return {
+        'docs': similar_documents,
+        'content': question
+    }
+
+def retrieve_similar_documents_from_database(question: str, threshold=0.5, num_docs=2):
+    
+    question_vector = embed_question(question)
+    conn = connect_to_postgresql()
+    cursor = conn.cursor()
+
+
+    sql_documents = f"""
+        SELECT name, lyrics, author, (embedding <=> \'{question_vector}\') AS distance
+        FROM songs
+        WHERE (embedding <=> \'{question_vector}\') > {threshold}
+        ORDER BY embedding <=> \'{question_vector}\'
+        LIMIT {num_docs}
+    """
+
+    try:
+        cursor.execute(sql_documents)
+        rows = cursor.fetchall()
+
+        # Filtra por threshold (distância menor = mais similar)
+        results = []
+        for row in rows:
+            name, lyrics, author, distance  = row
+            results.append({
+                "name": name,
+                "author": author,
+                "lyrics": lyrics,
+                "distance": distance
+            })
+
+        return results
+
+    except Exception as e:
+        print(f"Erro ao recuperar documentos: {e}")
+        return []
+    finally:
+        cursor.close()
+        conn.close()
+
 
 def generate_responses(question: str):
     normal_response = generate_response(question)
     rag_response = generate_rag_response(question)
 
     return {
-        'normal_response': normal_response,
+        'normal_response': {
+            'content': normal_response
+        },
         'rag_response': rag_response
     }
